364026103a0c03ed99485fbb04b161a881aef4c1
Evgeny40rus <hat-99@mail.ru>
2021-03-07 18:32:18 +0300

    Добавьте файлы проекта.

---
+++ Vivod_i_vvod_v_console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Vivod_i_vvod_v_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, Мир!");
+            Console.WriteLine();
+            Console.WriteLine("Меня зовут Евгения");
+        }
+    }
+}
