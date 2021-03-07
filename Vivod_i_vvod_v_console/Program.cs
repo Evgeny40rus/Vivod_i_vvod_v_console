275887e65052ef30b9f70429bef395e93a734e50
Evgeny40rus <hat-99@mail.ru>
2021-03-07 18:53:48 +0300

    Новая переменная возраст и строка с выводом

--- Vivod_i_vvod_v_console/Program.cs
+++ Vivod_i_vvod_v_console/Program.cs
@@ -4,13 +4,16 @@ namespace Vivod_i_vvod_v_console
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             string MyName = "Евгения";
+            byte age = 42;
 
             Console.WriteLine("Привет, Мир!");
             Console.WriteLine();
             Console.WriteLine($"Меня зовут {MyName}");
+            Console.WriteLine($"Мой возраст {age}");
+
 
             Console.ReadKey();
 
