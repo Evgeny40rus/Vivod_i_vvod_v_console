80409b1045ca0b28cf2697f50a9281f02da60dca
Evgeny40rus <hat-99@mail.ru>
2021-03-07 19:32:22 +0300

    Как указывать переменные в тексте

--- Vivod_i_vvod_v_console/Program.cs
+++ Vivod_i_vvod_v_console/Program.cs
@@ -1,22 +1,28 @@
 ﻿using System;
 
-namespace Vivod_i_vvod_v_console
-{
-    class Program
+class MainClass 
     {
-        public static void Main(string[] args)
+    public static void Main(string[] args)
         {
             string MyName = "Евгения";
-            byte age = 42;
+
+            byte ageEvg = 42;
 
             Console.WriteLine("Привет, Мир!");
             Console.WriteLine();
-            Console.WriteLine($"Меня зовут {MyName}");
-            Console.WriteLine($"Мой возраст {age}");
+            Console.WriteLine("Меня зовут {0} \n Мой возраст {1}" , MyName,ageEvg);
+
+        //Console.WriteLine($"Мой возраст {age}");
+
+        Console.WriteLine("Меня зовут {0}", "Маша");
+
+        var age = 27;
+        var weight = 50;
+
+        Console.WriteLine("Возраст {0} \n Вес {1}", age, weight);
 
 
             Console.ReadKey();
 
         }
     }
-}
